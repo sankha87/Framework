e1bde774407c3cffa6839bb7c18a2d58f5ad2dcc
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2023-06-22 00:36:53 +0530

    Test109 and Test110 updated with largest no and
    second largest no code(java)

--- Framework/src/challenges/Test109.java
+++ Framework/src/challenges/Test109.java
@@ -1,14 +1,33 @@
 package challenges;
 import java.util.Scanner;
+// largest of 3 nos.
 
 class Test109
 {
-	public static void main (String[] args)
-	{
-    	System.out.print("sample file ");
-    	System.out.print("sample file 2");
-	System.out.print("sample file 3");	
-  }
+	public static void main (String[] args) throws java.lang.Exception
+			{
+				Scanner r=new Scanner(System.in);
+				int t=r.nextInt();
+				for(int i=0;i<t;i++)
+				{
+				     int x=r.nextInt();
+				     int y=r.nextInt();
+				     int z=r.nextInt();
+				   
+				     if(x>y)
+				     {
+				    	 if(x>z)
+				    		 System.out.println(x);
+				    	 else
+				    		 System.out.println(z);
+				     }
+				     else	// x<y
+				     {
+				    	 if(y>z)
+				    		 System.out.println(y);
+				    	 else
+				    		 System.out.println(z); 
+				     }
+				}
+		}
 }
-
-  
--- Framework/src/challenges/Test110.java
+++ Framework/src/challenges/Test110.java
@@ -1,13 +1,41 @@
 package challenges;
 import java.util.Scanner;
 
+// second largest of 3 nos.
+
 class Test110
 {
-	public static void main (String[] args)
-	{
-    	System.out.print("sample file from another user");
-    	System.out.print("sample file from first user");
-  }
+	public static void main (String[] args) throws java.lang.Exception
+			{
+				Scanner r=new Scanner(System.in);
+				int t=r.nextInt();
+				for(int i=0;i<t;i++)
+				{
+				     int x=r.nextInt();
+				     int y=r.nextInt();
+				     int z=r.nextInt();
+				   
+				     if(x>y && x>z)			// x - largest
+				     {
+				    	 if(y>z)
+				    		 System.out.println(y);
+				    	 else
+				    		 System.out.println(z);
+				     }
+				     else if(y>x && y>z)	// y - largest
+				     {
+				    	 if(x>z)
+				    		 System.out.println(x);
+				    	 else
+				    		 System.out.println(z); 
+				     }
+				     else					// z - largest 			
+				     {
+				    	 if(x>y)
+				    		 System.out.println(x);
+				    	 else
+				    		 System.out.println(y);
+				     }
+				}
+		}
 }
-
-  
